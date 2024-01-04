fd41ae6a14bbf90a104f1763d86f0e8dae8749ea
21120561 <ducthinh12tn137@gmail.com>
2024-01-04 16:31:17 +0700

    update crud khuyen mai

--- BackEnd/routers/coupon.router.js
+++ BackEnd/routers/coupon.router.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getCoupons, applyCoupon, getCouponByCode } = require('../controller/coupon.controller');
+const { getCoupons, applyCoupon, getCouponByCode, addCoupon, getCouponPage, updateCoupon,deleteCoupon } = require('../controller/coupon.controller');
 
 router.get('/coupons', getCoupons);
 router.get('/apply-coupon', applyCoupon);
 router.get('/get-coupon-by-code', getCouponByCode);
+
+// Xu ly them, xoa, sua ma khuyen mai
+router.post('/coupon/add', addCoupon),
+router.get('/coupon/read', getCouponPage),
+router.post('/coupon/edit', updateCoupon),
+router.get('/coupon/delete', deleteCoupon),
 
 module.exports = router;
