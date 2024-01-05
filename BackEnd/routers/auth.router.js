68b63643e3259bb35ce8fa8bbb01ea13a2c72c4e
binzj195 <huuphuoc6ccc@gmail.com>
2024-01-05 21:52:09 +0700

    Session

--- BackEnd/routers/auth.router.js
+++ BackEnd/routers/auth.router.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 const {
     login,
-    logout
+    logout,
+    showLoginForm
 } = require('../controller/auth/login.controller');
 const {
+    create,
     register,
 } = require('../controller/auth/register.controller');
+
 const authMiddleWare = require('../Middlewares/auth.middleware');
+
 // router.post('/login', login);
 // router.post('/register', register);
+router.get('/login', authMiddleWare.isAuth, showLoginForm);
 router.post('/login', login);
+router.get('/register', authMiddleWare.isAuth, create);
 router.post('/register', register);
-router.get('/logout', authMiddleWare.loggedin, logout);
+router.post('/logout', authMiddleWare.loggedin, logout);
 module.exports = router;
